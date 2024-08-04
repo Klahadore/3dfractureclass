d1a276aa77b48fe9525b11a8488dc5874ab1273b
Quan Huynh <quanhuynh@Quans-MacBook-Pro.local>
2024-08-04 11:14:27 -0400

    running, but bad loss

--- TF_GroupUnet3d.py
+++ TF_GroupUnet3d.py
@@ -15,7 +15,7 @@ from groupy.gconv.tensorflow_gconv.splitgconv3d import gconv3d, gconv3d_util
 kernel_initializer = 'he_uniform'
 
 class GConv3D(tf.keras.layers.Layer):
-    def __init__(self, in_channels, out_channels, h_input, h_output, ksize=3, padding='SAME', activation='relu', **kwargs):
+    def __init__(self, in_channels, out_channels, h_input, h_output, groups = 16, ksize=3, padding='SAME', activation='relu', **kwargs):
         super(GConv3D, self).__init__(**kwargs)
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -24,6 +24,8 @@ class GConv3D(tf.keras.layers.Layer):
         self.ksize = ksize
         self.padding = padding
         self.activation = activation
+        self.groups = 16
+
         # Initialize gconv3d utility
         self.gconv_indices, self.gconv_shape_info, self.w_shape = gconv3d_util(
             h_input=self.h_input, h_output=self.h_output, in_channels=self.in_channels,
@@ -50,62 +52,62 @@ def group_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classes
     inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS))
     s = inputs
 
-    c1 = GConv3D(3, 16, "Z3", "OH")(s)
+    c1 = GConv3D(3, 2, "Z3", "D4H", groups = 16)(s)
     c1 = Dropout(0.1)(c1)
-    c1 = GConv3D(16, 16, "OH", "OH")(c1)
+    c1 = GConv3D(2, 2, "D4H", "D4H", groups = 16)(c1)
     print(c1.shape)
     p1 = MaxPooling3D((2,2,2))(c1)
     print(p1.shape)
 
-    c2 = GConv3D(16,32,"OH", "OH")(p1)
+    c2 = GConv3D(2,4,"D4H", "D4H", groups = 16)(p1)
     c2 = Dropout(0.1)(c2)
-    c2 = GConv3D(32,32, "OH", "OH")(c2)
+    c2 = GConv3D(4,4, "D4H", "D4H", groups = 16)(c2)
     p2 = MaxPooling3D((2,2,2))(c2)
 
-    c3 = GConv3D(32, 64, "OH", "OH")(p2)
+    c3 = GConv3D(4, 8, "D4H", "D4H", groups = 16)(p2)
     c3 = Dropout(0.1)(c3)
-    c3 = GConv3D(64, 64, "OH", "OH")(c3)
+    c3 = GConv3D(8, 8, "D4H", "D4H", groups = 16)(c3)
     p3 = MaxPooling3D((2,2,2))(c3)
 
-    c4 = GConv3D(64, 128, "OH", "OH")(p3)
+    c4 = GConv3D(8, 16, "D4H", "D4H", groups = 16)(p3)
     c4 = Dropout(0.1)(c4)
-    c4 = GConv3D(128, 128, "OH", "OH")(c4)
+    c4 = GConv3D(16, 16, "D4H", "D4H", groups = 16)(c4)
     p4 = MaxPooling3D((2,2,2))(c4)
 
-    c5 = GConv3D(128, 256, "OH", "OH")(p4)
+    c5 = GConv3D(16, 32, "D4H", "D4H", groups = 16)(p4)
     c5 = Dropout(0.1)(c5)
-    c5 = GConv3D(256, 256, "OH", "OH")(c5)
+    c5 = GConv3D(32, 32, "D4H", "D4H", groups = 16)(c5)
 
 
-    c6 = GConv3D(256, 128, "OH", "OH")(c5)
+    c6 = GConv3D(32, 16, "D4H", "D4H", groups = 16)(c5)
     c6 = UpSampling3D((2,2,2))(c6)
     #c6 = concatenate([c6, c4])
     c6 += c4
     c6 = Dropout(0.2)(c6)
-    c6 = GConv3D(128, 128, "OH", "OH")(c6)
+    c6 = GConv3D(16, 16, "D4H", "D4H", groups = 16)(c6)
     print(c6.shape)
-    c7 = GConv3D(128, 64, "OH", "OH")(c6)
+    c7 = GConv3D(16, 8, "D4H", "D4H", groups = 16)(c6)
     c7 = UpSampling3D((2,2,2))(c7)
     #c7 = concatenate([c7, c3])
     c7 += c3
     c7 = Dropout(0.2)(c7)
-    c7 = GConv3D(64, 64, "OH", "OH")(c7)
+    c7 = GConv3D(8, 8, "D4H", "D4H", groups = 16)(c7)
 
-    c8 = GConv3D(64, 32, "OH", "OH")(c7)
+    c8 = GConv3D(8, 4, "D4H", "D4H", groups = 16)(c7)
     c8 = UpSampling3D((2,2,2))(c8)
     #c8 = concatenate([c8,c2])
     c8 += c2
     c8 = Dropout(0.2)(c8)
-    c8 = GConv3D(32,32, "OH", "OH")(c8)
+    c8 = GConv3D(4,4, "D4H", "D4H", groups = 16)(c8)
 
-    c9 = GConv3D(32, 16, "OH", "OH")(c8)
+    c9 = GConv3D(4, 2, "D4H", "D4H", groups = 16)(c8)
     c9 = UpSampling3D((2,2,2))(c9)
     #c9 = concatenate([c9, c1])
     c9 += c1
     c9 = Dropout(0.2)(c9)
-    c9 = GConv3D(16, 16, "OH", "OH")(c9)
+    c9 = GConv3D(2, 2, "D4H", "D4H", groups = 16)(c9)
 
-    c9 = Reduce("b h w d (g c) -> b h w d c", g=48, reduction="sum")(c9)
+    c9 = Reduce("b h w d (g c) -> b h w d c", g=16, reduction="sum")(c9)
 
     outputs = Conv3D(num_classes, (1,1,1), activation='softmax')(c9)
 
